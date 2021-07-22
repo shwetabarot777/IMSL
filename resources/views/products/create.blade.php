f727b4e98033ab3c6b048ca9c76f581472c7361d
nima9443 <nimapatel9443@gmail.com>
2021-07-22 16:18:13 -0400

    Create functionality product image folder is not working..

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,4 +1,4 @@
-@extends('products.layout')
+@extends('layout.mainlayout')
 @section('content')
 <div class="row">
 	<div class="col-lg-12 margin-tb">
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -65,6 +65,8 @@
 			<form action="{{ route('products.destroy',$product->id) }}" method="POST">
 				<a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
 				<a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+				<a class="btn btn-warning" href="{{ route('products.images.index',$product->id) }}">Images</a>
+
 				@csrf
 				@method('DELETE')
 				<button type="submit" class="btn btn-danger">Delete</button>
